7db7e5a1320e57b200d2e7702e4cec6a14373a9b
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-26 11:04:58 +0530

    feat(model): create table `Features` inside database `ecommerce_dev`

---
+++ src/migrations/20210226053304-create-feature.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Features', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Features');
+  },
+};
