9de5e52214e946c67b69c76edd1f6cb1ac5c24c9
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-26 11:01:18 +0530

    feat(model): create table `Items` inside database `ecommerce_dev`

---
+++ src/migrations/20210226052801-create-item.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      item_id: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Items');
+  },
+};
